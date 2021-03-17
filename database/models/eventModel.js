7f4c4c73ff944e49fa51fcf16b650b9fa3d7bfd1
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-03-17 18:41:00 +1100

    middleware added: cors, errorHandling and payload + db schema

---
+++ database/models/eventModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: String,
+    message: String,
+    owner: String
+}, {
+    timestamps: true,
+    toObject: {
+        transform: function(doc, ret, options) {
+            delete ret.__v;
+            return ret;
+        }
+    }
+});
+
+module.exports = mongoose.model('Event', eventSchema)
--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const dotEnv = require('dotenv');
+const cors = require('cors');
+const dbConnection = require('./database/connection')
 
 dotEnv.config();
 
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// Mongo Database connection
+dbConnection()
 
-app.get('/', (req, res, next) => {
-    res.send('Hello from NodeCRUD api server')
-});
+// CORS Implementation
+app.use(cors());
+
+// payload middleware 
+app.use(express.json);
+app.use(express.urlencoded({extended: true})); /* request content type: "application/json"*/
+
+// PORT defination
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
-})
+});
+
+// // middleware: error handling
+// app.use(function (err, req, res, next) {
+//     console.error(err.stack)
+//     res.status(500).send({
+//         status: 500,
+//         message: err.message,
+//         body: {}
+//     });
+// });
